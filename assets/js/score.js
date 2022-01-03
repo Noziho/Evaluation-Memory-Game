30701eaa9351f852642352c75ee8406fe505fb00
Noah2211 <noah.decroix3@gmail.com>
2022-01-03 12:03:21 +0100

    score done

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,2 +1,6 @@
 let score = localStorage.getItem('score');
 document.getElementById('userScore').innerHTML += score + "/6";
+
+document.getElementById("reStart").addEventListener("click", () => {
+    window.location.assign('index.html');
+})
